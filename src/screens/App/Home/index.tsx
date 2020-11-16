2650da7787e5ba629585c6a5bb6d06635f043f61
zainzafar90 <zain@polly.ai>
2020-11-16 21:28:17 +0500

    Remove unused code from App and App Home

--- src/screens/App/Home/index.tsx
+++ src/screens/App/Home/index.tsx
@@ -1,24 +1,12 @@
-import React, { FC, useEffect } from 'react';
-import { Text, View, Button } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { FC } from 'react';
+import { Text, View } from 'react-native';
 
-import { RootState } from '@store/types';
-import { login } from '@store/auth';
 import { HomeScreenProps } from './types';
 
-const HomeScreen: FC<HomeScreenProps> = ({ navigation }) => {
-  const dispatch = useDispatch();
-  const { isAuthenticated, error } = useSelector((state: RootState) => state.auth);
-
-  useEffect(() => {
-    console.log(isAuthenticated, error);
-    dispatch(login('zainzafar90@gmail.com', '123456'));
-  }, []);
-
+const HomeScreen: FC<HomeScreenProps> = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Button title="Toggle Drawer" onPress={() => navigation.toggleDrawer()} />
     </View>
   );
 };
